a5d107b8443610d7b2a558893086e6a030f056ba
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2022-10-31 21:15:05 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$server  ="localhost";
+$username="root" ;
+$password="";
+$db= "db_web";
+$koneksi= mysqli_connect($server,$username,$password,$db);
+
+//pastikan urutan pemanggilan vriabel sama 
+
+//untuk cek jika koneksi gagal ke databse 
+?>
---
+++ login.php
@@ -0,0 +1,154 @@
+<?php
+require('koneksi.php');
+session_start();
+if( isset($_POST['submit'])){
+    $email = $_POST['txt_email'];
+    $pass  = $_POST['txt_pass'];
+    
+    //$emailCheck = mysqli_real_escape_string($koneksi, $email);
+    //$passCheck  = mysqli_real_escape_string($koneksi, $pass);
+    
+    if(!empty(trim($email)) && !empty(trim($pass))){
+        //select data berdasarkan username dari database
+        $query  ="SELECT * FROM user_detail WHERE user_email = '$email'";
+        $result = mysqli_query($koneksi,$query);
+        $num    = mysqli_num_rows($result);
+
+        while ($row = mysqli_fetch_array($result)){
+            $id = $row['id'];
+            $userVal = $row['user_email'];
+            $passVal = $row['user_password'];
+            $userName = $row['user_fullname'];
+            $level = $row['level'];
+        }
+        if ($num !=0){
+            if($userVal==$email && $passVal==$pass){
+                $_SESSION['id'] = $id;
+                $_SESSION['name'] = $userName;
+                $_SESSION['level'] = $level;
+                header('Location: index.php');
+            }else{
+                $error= 'user atau password salah!!';
+                echo'<div id="tampil_modal">
+                     <div id="modal">
+                    <div id="modal_atas">Informasi</div>
+                    <p>Gagal di tambahkan!.</p>
+                    <a href="index.php"><button id="oke">Oke</button></a>
+                    </div></div>';
+            }
+        }else{
+            $error = 'user tidak di temukan!!';
+            echo $error;
+        }
+    }else{
+        $error = 'Data tidak boleh kosong!!';
+        echo $error;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+        <link rel="stylesheet" href="tes.css">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="login.php">
+                                        <div class="form-group">
+                                            <input type="email" name="txt_email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="txt_pass" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <button name="submit" type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                        <hr>
+                                        <a href="login.php" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="login.php" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.php">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+<script>
+</script>
+
+</body>
+
+</html>
+
